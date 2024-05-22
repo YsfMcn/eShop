391ae944a71ac93a4130bd8d80908f370d8d350b
YsfMcn <mcnysf@gmail.com>
2024-05-22 17:59:12 +0300

    project initial structure

---
+++ eShop/src/test/java/com/UnitTestingSpring/eShop/EShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UnitTestingSpring.eShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
